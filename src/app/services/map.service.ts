f2b4089ba3aca20a2c8169b6fb9daf84835343f2
haoliangyu <njkon@foxmail.com>
2018-07-24 22:46:52 -0400

    remove some basemaps

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -39,39 +39,17 @@ export class MapService {
   }
 
   createMap(elementId: string): void {
-    const osmAttr =
-      "&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, " +
-      "Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>";
-
     const esriAttr =
       "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, " +
       "iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, " +
       "Esri China (Hong Kong), and the GIS User Community";
 
-    const cartoAttr =
-      "&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> " +
-      "&copy; <a href="http://cartodb.com/attributions">CartoDB</a>";
-
     const baseMaps = {
-      OpenStreetMap: L.tileLayer(
-        "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-        {
-          zIndex: 1,
-          attribution: osmAttr
-        }
-      ),
       Esri: L.tileLayer(
         "http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
         {
           zIndex: 1,
           attribution: esriAttr
-        }
-      ),
-      CartoDB: L.tileLayer(
-        "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
-        {
-          zIndex: 1,
-          attribution: cartoAttr
         }
       )
     };
