a7874783aa6e211f6745661abda0ddd28b9a07c4
Haoliang Yu <haoliangyu@users.noreply.github.com>
2018-07-25 21:49:28 -0400

    add join route (#17)

--- src/app/editor-page/editor-page.component.ts
+++ src/app/editor-page/editor-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, AfterViewInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Router, Params } from "@angular/router";
 import { MatSnackBar } from "@angular/material";
 import { LoadingService } from "../services/loading.service";
 import { DbService } from "../services/db.service";
@@ -13,6 +13,7 @@ export class EditorPageComponent implements OnInit, AfterViewInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private db: DbService,
     private loading: LoadingService,
     private snackBar: MatSnackBar
@@ -36,13 +37,26 @@ export class EditorPageComponent implements OnInit, AfterViewInit {
       }
 
       actionFunc = this.createMap.bind(this, name);
+    } else if (action === "join") {
+      const address = decodeURIComponent(this.route.snapshot.queryParams.address);
+
+      if (!address) {
+        this.snackBar.open("Map address is required!", "", { duration: 2000 });
+        return;
+      }
+
+      actionFunc = this.joinMap.bind(this, address);
+    } else {
+      return this.router.navigateByUrl("/");
     }
 
     if (actionFunc && this.db.ready) {
       await actionFunc();
+      this.router.navigateByUrl("/");
     } else if (actionFunc && !this.db.ready) {
       this.db.events.dbReady.subscribe(async () => {
         await actionFunc();
+        this.router.navigateByUrl("/");
       });
     }
   }
@@ -53,6 +67,19 @@ export class EditorPageComponent implements OnInit, AfterViewInit {
       await this.db.createMap(name);
 
       this.snackBar.open("You create a new map!", "", { duration: 2000 });
+    } catch (error) {
+      console.log(error);
+    }
+
+    this.loading.endLoading();
+  }
+
+  private async joinMap (address: string) {
+    try {
+      this.loading.startLoading();
+      await this.db.joinMap(address);
+
+      this.snackBar.open("You join a new map!", "", { duration: 2000 });
     } catch (error) {
       console.log(error);
     }
