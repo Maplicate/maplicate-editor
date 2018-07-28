d59245d8fadbcd100f872fe1ee85592b91839772
Haoliang Yu <haoliangyu@users.noreply.github.com>
2018-07-27 22:46:36 -0400

    add share button (#18)

---
+++ src/app/components/share-dialog/share-dialog.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Inject } from "@angular/core";
+import { MatSnackBar, MAT_DIALOG_DATA } from "@angular/material";
+
+@Component({
+  selector: "app-share-dialog",
+  templateUrl: "./share-dialog.component.html",
+  styleUrls: ["./share-dialog.component.css"]
+})
+export class ShareDialogComponent implements OnInit {
+  public joinLink: string;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) private data: any
+  ) {
+    this.joinLink = data.joinLink;
+  }
+
+  ngOnInit() {}
+
+  private copyLink() {
+    this.snackBar.open("Share link is copied to the clipboard.", "", {
+      duration: 2000
+    });
+  }
+}
