3d700f39585a7c9a7f7a17715d63074c53413910
haoliangyu <njkon@foxmail.com>
2018-09-12 19:25:30 +0800

    move files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { ClipboardModule } from "ngx-clipboard";
-import { NgMaterialModule } from "./ng-material/ng-material.module";
-import { RoutingModule } from "./routing/routing.module";
+import { NgMaterialModule } from "./modules/ng-material/ng-material.module";
+import { RoutingModule } from "./modules/routing/routing.module";
 
 import { AppComponent } from "./app.component";
 import { MapComponent } from "./components/map/map.component";
 import { ToolbarComponent } from "./components/toolbar/toolbar.component";
-import { CreateMapDialogComponent } from "./create-map-dialog/create-map-dialog.component";
-import { JoinMapDialogComponent } from "./join-map-dialog/join-map-dialog.component";
-import { ShareDialogComponent } from "./components/share-dialog/share-dialog.component";
-import { PropertyEditFormComponent } from "./property-edit-form/property-edit-form.component";
-import { EditorPageComponent } from "./pages/editor-page/editor-page.component";
+import { CreateMapDialogComponent } from "./components/dialogs/create-map-dialog/create-map-dialog.component";
+import { JoinMapDialogComponent } from "./components/dialogs/join-map-dialog/join-map-dialog.component";
+import { ShareDialogComponent } from "./components/dialogs/share-dialog/share-dialog.component";
+import { PropertyEditFormComponent } from "./components/property-edit-form/property-edit-form.component";
+import { EditorPageComponent } from "./components/pages/editor-page/editor-page.component";
 
 import { MapControlService } from "./services/map-control.service";
 import { DbService } from "./services/db.service";
